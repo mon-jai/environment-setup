280bd10a0959aa6e39a4e1ef7701e47c0b9b7625
mon-jai <91261297+mon-jai@users.noreply.github.com>
2023-01-06 00:08:09 +0800

    Update setup-environment.ps1

--- setup-environment.ps1
+++ setup-environment.ps1
@@ -115,11 +115,11 @@ Start-Job -Name 'Configure VSCode' -ScriptBlock {
   New-Item $vscodeSettingsDir -ItemType Directory -ErrorAction SilentlyContinue *>&1 | Out-Null
   ConvertTo-Json -InputObject $vscodeSettings | Out-File -Encoding "UTF8" "$vscodeSettingsDir\settings.json"
 
-  code --install-extension formulahendry.code-runner --force | Out-Null
-  code --install-extension github.github-vscode-theme --force | Out-Null
+  code --install-extension formulahendry.code-runner --force *>&1 | Out-Null
+  code --install-extension github.github-vscode-theme --force *>&1 | Out-Null
   
   if ($InstallPython) {
-    code --install-extension ms-python.python --force | Out-Null
+    code --install-extension ms-python.python --force *>&1 | Out-Null
   }
 
   Write-Host "Configured VSCode"
