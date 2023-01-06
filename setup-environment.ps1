3437718e54a8476667c6d4dd8277169fb9009c6e
mon-jai <91261297+mon-jai@users.noreply.github.com>
2023-01-07 04:20:36 +0800

    Update setup-environment.ps1

--- setup-environment.ps1
+++ setup-environment.ps1
@@ -13,7 +13,7 @@ function Out-Log {
   [CmdletBinding()]
   Param ([Parameter(ValueFromPipeline)] [string[]]$content)
   Process {
-    # $content | Out-File -Append -LiteralPath $logFilePath
+    $content | Out-File -Append -LiteralPath $logFilePath
   }
 }
 
@@ -44,98 +44,98 @@ Start-Job -Name 'Configure language' -ErrorAction Stop -ScriptBlock {
   Write-Host "Configured language"
 }
 
-# Start-Job -Name 'Install Windows Terminal' -ErrorAction Stop -ScriptBlock {
-#   $desktopFrameworkPackageDownloadURL = "https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx"
-#   $desktopFrameworkPackageDownloadPath = "$Env:TEMP/VCLibs.appx"
+Start-Job -Name 'Install Windows Terminal' -ErrorAction Stop -ScriptBlock {
+  $desktopFrameworkPackageDownloadURL = "https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx"
+  $desktopFrameworkPackageDownloadPath = "$Env:TEMP/VCLibs.appx"
 
-#   $windowsTerminalDownloadURL = "https://github.com/microsoft/terminal/releases/download/v1.15.3465.0/Microsoft.WindowsTerminal_Win10_1.15.3465.0_8wekyb3d8bbwe.msixbundle"
-#   $windowsTerminalDownloadPath = "$Env:TEMP/WindowsTerminal.msixbundle"
+  $windowsTerminalDownloadURL = "https://github.com/microsoft/terminal/releases/download/v1.15.3465.0/Microsoft.WindowsTerminal_Win10_1.15.3465.0_8wekyb3d8bbwe.msixbundle"
+  $windowsTerminalDownloadPath = "$Env:TEMP/WindowsTerminal.msixbundle"
 
-#   Start-BitsTransfer $desktopFrameworkPackageDownloadURL $desktopFrameworkPackageDownloadPath
-#   Start-BitsTransfer $windowsTerminalDownloadURL $windowsTerminalDownloadPath
+  Start-BitsTransfer $desktopFrameworkPackageDownloadURL $desktopFrameworkPackageDownloadPath
+  Start-BitsTransfer $windowsTerminalDownloadURL $windowsTerminalDownloadPath
 
-#   try {
-#     Add-AppxPackage $desktopFrameworkPackageDownloadPath -ErrorAction Stop *>&1 | Out-Log
-#     Add-AppxPackage $windowsTerminalDownloadPath -ErrorAction Stop *>&1 | Out-Log
+  try {
+    Add-AppxPackage $desktopFrameworkPackageDownloadPath -ErrorAction Stop *>&1 | Out-Log
+    Add-AppxPackage $windowsTerminalDownloadPath -ErrorAction Stop *>&1 | Out-Log
 
-#     Write-Host "Installed Windows Terminal"
-#   }
-#   catch {
-#     Write-Host "Install Windows Terminal skipped"
-#   }
-# }
+    Write-Host "Installed Windows Terminal"
+  }
+  catch {
+    Write-Host "Install Windows Terminal skipped"
+  }
+}
 
-# if ($InstallPython) {
-#   Start-Job -Name 'Install and configure Python' -ErrorAction Stop -ScriptBlock {
-#     $pythonDownloadPath = "$Env:TEMP/python.exe"
+if ($InstallPython) {
+  Start-Job -Name 'Install and configure Python' -ErrorAction Stop -ScriptBlock {
+    $pythonDownloadPath = "$Env:TEMP/python.exe"
 
-#     # https://stackoverflow.com/a/73534796
-#     if (
-#       (Invoke-RestMethod 'https://www.python.org/downloads/') -notmatch
-#       '\bhref="(?<url>.+?\.exe)"\s*>\s*Download Python (?<version>\d+\.\d+\.\d+)'
-#     ) { throw "Could not determine latest Python version and download URL" }
+    # https://stackoverflow.com/a/73534796
+    if (
+      (Invoke-RestMethod 'https://www.python.org/downloads/') -notmatch
+      '\bhref="(?<url>.+?\.exe)"\s*>\s*Download Python (?<version>\d+\.\d+\.\d+)'
+    ) { throw "Could not determine latest Python version and download URL" }
 
-#     # https://stackoverflow.com/a/21423159
-#     Start-BitsTransfer $Matches.url $pythonDownloadPath
+    # https://stackoverflow.com/a/21423159
+    Start-BitsTransfer $Matches.url $pythonDownloadPath
 
-#     # https://stackoverflow.com/a/73665900
-#     Start-Process $pythonDownloadPath -ArgumentList "/quiet", "PrependPath=1", "InstallLauncherAllUsers=0" -NoNewWindow -Wait
-#     Remove-Item $pythonDownloadPath
+    # https://stackoverflow.com/a/73665900
+    Start-Process $pythonDownloadPath -ArgumentList "/quiet", "PrependPath=1", "InstallLauncherAllUsers=0" -NoNewWindow -Wait
+    Remove-Item $pythonDownloadPath
 
-#     # Reload PATH to run python and pip, https://stackoverflow.com/a/31845512
-#     $Env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
+    # Reload PATH to run python and pip, https://stackoverflow.com/a/31845512
+    $Env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
 
-#     # https://stackoverflow.com/a/67796873
-#     pip config set global.trusted-host "pypi.org files.pythonhosted.org pypi.python.org" | Out-Log
-#     python -m pip install --upgrade pip | Out-Log
-#     pip install -U autopep8 | Out-Log
+    # https://stackoverflow.com/a/67796873
+    pip config set global.trusted-host "pypi.org files.pythonhosted.org pypi.python.org" | Out-Log
+    python -m pip install --upgrade pip | Out-Log
+    pip install -U autopep8 | Out-Log
 
-#     Write-Host "Installed and configured Python"
-#   }
-# }
+    Write-Host "Installed and configured Python"
+  }
+}
 
-# Start-Job -Name 'Configure VSCode' -ErrorAction Stop -ScriptBlock {
-#   # https://stackoverflow.com/a/36705460
-#   # https://stackoverflow.com/a/36751445
-#   Remove-Item "$Env:USERPROFILE/.vscode/extensions" -Force -Recurse -ErrorAction SilentlyContinue
+Start-Job -Name 'Configure VSCode' -ErrorAction Stop -ScriptBlock {
+  # https://stackoverflow.com/a/36705460
+  # https://stackoverflow.com/a/36751445
+  Remove-Item "$Env:USERPROFILE/.vscode/extensions" -Force -Recurse -ErrorAction SilentlyContinue
 
-#   $vscodeSettingsDir = "$Env:APPDATA\Code\User\"
-#   $vscodeSettings = [pscustomobject]@{
-#     "[python]"                         = [pscustomobject]@{
-#       "editor.tabSize" = 4
-#     }
-#     "code-runner.clearPreviousOutput"  = $true
-#     "code-runner.executorMap"          = [pscustomobject]@{
-#       # https://stackoverflow.com/a/53961913
-#       "python" = "clear; & `"`$env:LocalAppData\Programs\Python\Python311\python`" -u"
-#     }
-#     "code-runner.ignoreSelection"      = $true
-#     "code-runner.runInTerminal"        = $true
-#     "code-runner.saveFileBeforeRun"    = $true
-#     "editor.tabSize"                   = 2
-#     "explorer.confirmDelete"           = $false
-#     "files.associations"               = [pscustomobject]@{
-#       "*.xml" = "html"
-#     }
-#     "http.proxyStrictSSL"              = $false
-#     "python.analysis.typeCheckingMode" = "strict"
-#     "workbench.colorTheme"             = "GitHub Light Default"
-#     "workbench.startupEditor"          = "none"
-#   }
+  $vscodeSettingsDir = "$Env:APPDATA\Code\User\"
+  $vscodeSettings = [pscustomobject]@{
+    "[python]"                         = [pscustomobject]@{
+      "editor.tabSize" = 4
+    }
+    "code-runner.clearPreviousOutput"  = $true
+    "code-runner.executorMap"          = [pscustomobject]@{
+      # https://stackoverflow.com/a/53961913
+      "python" = "clear; & `"`$env:LocalAppData\Programs\Python\Python311\python`" -u"
+    }
+    "code-runner.ignoreSelection"      = $true
+    "code-runner.runInTerminal"        = $true
+    "code-runner.saveFileBeforeRun"    = $true
+    "editor.tabSize"                   = 2
+    "explorer.confirmDelete"           = $false
+    "files.associations"               = [pscustomobject]@{
+      "*.xml" = "html"
+    }
+    "http.proxyStrictSSL"              = $false
+    "python.analysis.typeCheckingMode" = "strict"
+    "workbench.colorTheme"             = "GitHub Light Default"
+    "workbench.startupEditor"          = "none"
+  }
 
-#   # Throw an error if the directory already exists
-#   New-Item $vscodeSettingsDir -ItemType Directory -ErrorAction SilentlyContinue *>&1 | Out-File -Append $logFilePath
-#   ConvertTo-Json -InputObject $vscodeSettings | Out-File -Encoding "UTF8" "$vscodeSettingsDir\settings.json"
+  # Throw an error if the directory already exists
+  New-Item $vscodeSettingsDir -ItemType Directory -ErrorAction SilentlyContinue *>&1 | Out-Log
+  ConvertTo-Json -InputObject $vscodeSettings | Out-File -Encoding "UTF8" "$vscodeSettingsDir\settings.json"
 
-#   # code --install-extension formulahendry.code-runner --force *>&1 | Out-Null
-#   # code --install-extension github.github-vscode-theme --force *>&1 | Out-Null
+  code --install-extension formulahendry.code-runner --force *>&1 | Out-Null
+  code --install-extension github.github-vscode-theme --force *>&1 | Out-Null
 
-#   # if ($InstallPython) {
-#   #   code --install-extension ms-python.python --force *>&1 | Out-Null
-#   # }
+  if ($InstallPython) {
+    code --install-extension ms-python.python --force *>&1 | Out-Null
+  }
 
-#   Write-Host "Configured VSCode"
-# }
+  Write-Host "Configured VSCode"
+}
 
 Get-Job | Receive-Job -Wait -ErrorAction Stop
 Write-Host "Done!"
