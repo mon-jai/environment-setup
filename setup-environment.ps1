d91df615b33c9ceef9fbc397ebf6712a8bd4cfff
mon-jai <91261297+mon-jai@users.noreply.github.com>
2023-01-17 01:57:28 +0800

    Update setup-environment.ps1

--- setup-environment.ps1
+++ setup-environment.ps1
@@ -33,6 +33,8 @@ $add_custom_cmdlet = {
   }
 }
 
+. $add_custom_cmdlet
+
 Start-Job -Name 'Enable clipboard' -InitializationScript $add_custom_cmdlet -ScriptBlock {
   try {
     # https://stackoverflow.com/a/41476689
@@ -43,7 +45,7 @@ Start-Job -Name 'Enable clipboard' -InitializationScript $add_custom_cmdlet -Scr
   catch {
     Write-Host-And-Log "Enable clipboard skipped"
   }
-}
+} | Write-Host-And-Log # https://stackoverflow.com/a/58809009/11077662
 
 Start-Job -Name 'Configure language' -InitializationScript $add_custom_cmdlet -ScriptBlock {
   # https://stackoverflow.com/a/51374938
@@ -58,7 +60,7 @@ Start-Job -Name 'Configure language' -InitializationScript $add_custom_cmdlet -S
   Set-WinUserLanguageList $languageList -Force
 
   Write-Host-And-Log "Configured language"
-}
+} | Write-Host-And-Log
 
 Start-Job -Name 'Install Windows Terminal' -InitializationScript $add_custom_cmdlet -ScriptBlock {
   $desktopFrameworkPackageDownloadURL = "https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx"
@@ -79,7 +81,7 @@ Start-Job -Name 'Install Windows Terminal' -InitializationScript $add_custom_cmd
   catch {
     Write-Host-And-Log "Install Windows Terminal skipped"
   }
-}
+} | Write-Host-And-Log
 
 if ($InstallPython) {
   Start-Job -Name 'Install and configure Python' -InitializationScript $add_custom_cmdlet -ScriptBlock {
@@ -107,7 +109,7 @@ if ($InstallPython) {
     pip install -U autopep8 | Write-Log
 
     Write-Host-And-Log "Installed and configured Python"
-  }
+  } | Write-Host-And-Log
 }
 
 Start-Job -Name 'Configure VSCode' -InitializationScript $add_custom_cmdlet -ScriptBlock {
@@ -151,11 +153,8 @@ Start-Job -Name 'Configure VSCode' -InitializationScript $add_custom_cmdlet -Scr
   }
 
   Write-Host-And-Log "Configured VSCode"
-}
+} | Write-Host-And-Log
 
-. $add_custom_cmdlet
+Get-Job | Receive-Job -Wait -ErrorAction Stop
 
-# https://stackoverflow.com/a/58809009
-Get-Job *>&1 | Receive-Job -Wait -ErrorAction Stop *>&1 | Out-Null
-
-Write-Host-And-Log "Done!"
+Write-Host "Done!"
